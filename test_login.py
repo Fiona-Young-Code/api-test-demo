6cddadbe4f205dd0b073b6a4aca9958efe2c9baa
Fiona-Young-Code <luoluoyang24@gmail.com>
2026-04-12 10:18:01 +0800

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,74 +1,16 @@
-# import requests
-#
-# def test_login_success():
-#     url = "https://reqres.in/api/login"
-#
-#     payload = {
-#         "email": "eve.holt@reqres.in",
-#         "password": "cityslicka"
-#     }
-#
-#     response = requests.post(url, json=payload)
-#
-#     # 状态码校验
-#     assert response.status_code == 200
-#
-#     res = response.json()
-#
-#     # 业务校验
-#     assert "token" in res
-#
-# def test_login_failed_without_password():
-#     url = "https://reqres.in/api/login"
-#     payload = {
-#         "email": "eve.holt@reqres.in"
-#     }
-#
-#     response = requests.post(url, json=payload)
-#
-#     # 状态码校验
-#     assert response.status_code == 400
-#
-#     res = response.json()
-#
-#     # 业务校验
-#     assert "error" in res
-import requests
+import pytest
+from api.login_api import login
+from utils.data_loader import load_login_test_data
 
+login_test_data = load_login_test_data()
 
-def test_login_success():
-    url = "https://httpbin.org/post"
-
-    payload = {
-        "username": "testuser",
-        "password": "123456"
-    }
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    print(response.status_code)
-    print(response.text)
+@pytest.mark.parametrize("case", login_test_data, ids=[case["title"] for case in login_test_data])
+def test_login_success(case, common_headers):
+    response = login(case["username"], case["password"], common_headers)
 
     assert response.status_code == 200
 
     res = response.json()
-    assert res["json"]["username"] == "testuser"
-
-
-def test_login_failed_without_password():
-    url = "https://httpbin.org/post"
-
-    payload = {
-        "email": "testuser"
-    }
-
-    response = requests.post(url, json=payload)
-
-    print(response.status_code)
-    print(response.text)
-
-    assert response.status_code == 200
+    assert "json" in res
+    assert res["json"]["username"] == case["username"]
+    assert res["json"]["password"] == case["password"]
