49e8c47ca3281ba5a0f5e050ef73465af9cf24b9
Fiona-Young-Code <luoluoyang24@gmail.com>
2026-04-12 10:52:05 +0800

    Update login_api.py

--- api/login_api.py
+++ api/login_api.py
@@ -1,10 +1,14 @@
 import requests
 from config.config import BASE_URL,LOGIN_PATH
+from utils.logger import get_logger
 
+logger = get_logger()
 def login(username, password, headers=None):
     url =BASE_URL + LOGIN_PATH 
     payload = {
         "username": username,
         "password": password
     }
+    logger.info(f"Request URL: {url}")
+    logger.info(f"Request Payload: {payload}")
     return requests.post(url, json=payload)
